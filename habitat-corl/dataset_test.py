7b69b8131016996d0a259313b3e5235ef1a5214f
AlexanderBelooussov <alexander.belooussov@student.uantwerpen.be>
2023-02-24 19:27:06 +0100

    testing dataset loading

---
+++ habitat-corl/dataset_test.py
@@ -0,0 +1,41 @@
+from habitat import Config
+from habitat_baselines.config.default import get_config
+from habitat_baselines.common.environments import get_env_class
+from habitat_baselines.common.obs_transformers import \
+    apply_obs_transforms_obs_space, get_active_obs_transforms
+from habitat_baselines.il.env_based.common.rollout_storage import \
+    RolloutStorage
+from habitat_baselines.utils.env_utils import construct_envs
+
+
+def main():
+    config_path = "habitat-corl/configs/bc_objectnav.yaml"
+    config = get_config(config_path, [])
+    print(f"env")
+    envs = construct_envs(
+        config, get_env_class(config.ENV_NAME)
+    )
+
+    bc_config = config.ORL.BC
+
+    print(f"observation space")
+    observation_space = envs.observation_spaces[0]
+    obs_transforms = get_active_obs_transforms(bc_config)
+    observation_space = apply_obs_transforms_obs_space(
+        observation_space, obs_transforms
+    )
+    obs_space = observation_space
+
+    print(f"rollouts")
+    rollouts = RolloutStorage(
+        bc_config.num_steps,
+        envs.num_envs,
+        obs_space,
+        envs.action_spaces[0],
+        config.MODEL.STATE_ENCODER.hidden_size,
+        config.MODEL.STATE_ENCODER.num_recurrent_layers,
+    )
+    print("generated dataset succesfully!")
+
+if __name__ == "__main__":
+    main()
