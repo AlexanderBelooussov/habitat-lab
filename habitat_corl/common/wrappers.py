fixes to state normalization

habitat_corl/common/wrappers.py
@@ -126,7 +126,7 @@ def wrap_env(
         return np.concatenate([state[key] for key in used_inputs], axis=-1)
 
     def normalize_state(state):
-        return (state - state_mean) / state_std
+        return (state - state_mean) / (state_std + 1e-8)
 
     def transform_state(state):
         raw_state = deepcopy(state)

habitat_corl/iql.py
@@ -399,9 +399,9 @@ def train(config):
             if algo_config.iql_deterministic
             else GaussianPolicy(state_dim, action_dim, max_action)
         ).to(device)
-        v_optimizer = torch.optim.Adam(v_network.parameters(), lr=3e-6)
-        q_optimizer = torch.optim.Adam(q_network.parameters(), lr=3e-6)
-        actor_optimizer = torch.optim.Adam(actor.parameters(), lr=3e-6)
+        v_optimizer = torch.optim.Adam(v_network.parameters(), lr=3e-5)
+        q_optimizer = torch.optim.Adam(q_network.parameters(), lr=3e-5)
+        actor_optimizer = torch.optim.Adam(actor.parameters(), lr=3e-5)
 
         kwargs = {
             "max_action": max_action,

habitat_corl/shortest_path_dataset.py
@@ -318,7 +318,8 @@ def generate_shortest_path_dataset(config, train_episodes=None,
 
 def load_full_dataset(config, groups=None, datasets=None, continuous=False,
                       ignore_stop=False, single_goal=None,
-                      frac=1.0, discount=0.99, max_episode_steps=1000):
+                      frac=1.0, discount=0.99, max_episode_steps=1000,
+                      normalization_data=None):
     if datasets is None:
         datasets = [
             "states/position",
@@ -355,6 +356,8 @@ def load_full_dataset(config, groups=None, datasets=None, continuous=False,
         rpb.to_continuous_actions()
 
     rpb.to_numpy()
+    if normalization_data is not None:
+        rpb.normalize_states(normalization_data)
 
     if frac < 1.0:
         # keep the best trajectories (for BC-x%)
@@ -470,7 +473,8 @@ def batch_generator(
     single_goal=None,
     frac=1.0,
     discount=0.99,
-    max_episode_steps=1000
+    max_episode_steps=1000,
+    normalization_data=None
 ):
     if continuous:
         ignore_stop = True
@@ -498,7 +502,8 @@ def batch_generator(
             single_goal=single_goal,
             frac=frac,
             discount=discount,
-            max_episode_steps=max_episode_steps
+            max_episode_steps=max_episode_steps,
+            normalization_data=normalization_data
         )
         while True:
             yield dataset.sample(n_transitions)
@@ -559,6 +564,11 @@ def calc_mean_std(config, groups=None, used_inputs=None):
                             std = new_std
             all_data = np.concatenate(all_data, axis=0)
             stats[ds] = (np.mean(all_data, axis=0), np.std(all_data, axis=0))
+
+    if "state_heading_vec" in stats:
+        stats["state_heading_vec"] = (np.zeros(2), np.ones(2))
+    if "state_position" in stats and "state_goal_position" in stats:
+        stats["state_goal_position"] = stats["state_position"]
 
     if used_inputs is not None:
         used_mean = []

habitat_corl/td3_bc.py
@@ -271,7 +271,8 @@ def train(config):
                      [f"next_state_{x}" for x in config.MODEL.used_inputs] + \
                      ["action", "reward", "done"],
             continuous=True,
-            single_goal=get_goal(algo_config, eval_episodes)
+            single_goal=get_goal(algo_config, eval_episodes),
+            normalization_data=mean_std
         )
 
         max_action = float(env.action_space.high[0])
@@ -289,12 +290,12 @@ def train(config):
         set_seed(seed, env)
 
         actor = Actor(state_dim, action_dim, max_action).to(device)
-        actor_optimizer = torch.optim.Adam(actor.parameters(), lr=3e-5)
+        actor_optimizer = torch.optim.Adam(actor.parameters(), lr=3e-4)
 
         critic_1 = Critic(state_dim, action_dim).to(device)
-        critic_1_optimizer = torch.optim.Adam(critic_1.parameters(), lr=3e-5)
+        critic_1_optimizer = torch.optim.Adam(critic_1.parameters(), lr=3e-4)
         critic_2 = Critic(state_dim, action_dim).to(device)
-        critic_2_optimizer = torch.optim.Adam(critic_2.parameters(), lr=3e-5)
+        critic_2_optimizer = torch.optim.Adam(critic_2.parameters(), lr=3e-4)
 
         wandb.watch(actor)
         wandb.watch(critic_1)
@@ -336,7 +337,6 @@ def train(config):
         evaluations = []
         for t in trange(int(algo_config.max_timesteps), desc="Training"):
             batch = next(batch_gen)
-            batch.normalize_states(mean_std)
             log_dict = trainer.train(batch, used_inputs=config.MODEL.used_inputs)
             wandb.log(log_dict, step=trainer.total_it)
             # Evaluate episode